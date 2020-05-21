529c2ba3517e618ea2ead273ba2bf4399d60fbb3
potterrules <62042288+potterrules@users.noreply.github.com>
2020-05-20 21:56:56 -0500

    correcion gabo el perron

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-            Gabriel el chido
+            
         <!-- Fonts fuente -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Gabriel
+                    Gabriel es chido
                 </div>
 
                 <div class="links">
